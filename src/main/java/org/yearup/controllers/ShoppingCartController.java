da4a9e9b7a41e797e459570f19b7d14662503df9
kay8181 <you@example.com>
2026-06-25 23:54:57 -0700

    Fixed GET and POST

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -16,8 +16,8 @@ import java.security.Principal;
 // convert this class to a REST controller
 // only logged in users should have access to these actions
 @RestController
-@RequestMapping("/cart")
-@CrossOrigin
+@RequestMapping("cart")
+@CrossOrigin(origins="")
 public class ShoppingCartController
 {
     // a shopping cart controller depends on the service layer
@@ -50,23 +50,31 @@ public class ShoppingCartController
     // add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15  (15 is the productId to be added)
     // return the updated cart with status 201 Created
-    @PostMapping("/products/{id}")
+    @PostMapping("products/{id}")
     @PreAuthorize("hasRole('ROLE_USER')")
-    public ShoppingCart addToCart(Principal principal, @PathVariable int id, @RequestBody ShoppingCart shoppingCart)
+//    public ShoppingCart addToCart(Principal principal, @PathVariable int id, @RequestBody ShoppingCart shoppingCart)
+    public ResponseEntity<ShoppingCart> addToCart (Principal principal, @PathVariable int id, @RequestBody(required = false) ShoppingCart ignoredBody)
     {
-        shoppingCart = getCart(principal);
         if (productService.getById(id) == null)
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 
-        return shoppingCartService.addToCart(shoppingCart, id);
-        //return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+        String userName = principal.getName();
+        // find database user by username
+        User user = userService.getByUserName(userName);
+        int userId = user.getId();
+
+        ShoppingCart cart = getCart(principal);
+        ShoppingCart saved = shoppingCartService.addToCart(cart, id, userId);
+
+//        return shoppingCartService.addToCart(shoppingCart, id);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
     }
 
 
     // add a PUT method to update an existing product in the cart - the url should be
     // https://localhost:8080/cart/products/15  (15 is the productId to be updated)
     // the BODY should be a ShoppingCartItem - quantity is the only value that will be updated; return the cart (200 OK)
-    @PutMapping("/cart/products/{id}")
+    @PutMapping("/products/{id}")
     @PreAuthorize("hasRole('ROLE_USER')")
     public ShoppingCart updateItemInCart(Principal principal, @PathVariable int id, @RequestBody ShoppingCartItem shoppingCartItem, ShoppingCart shoppingCart, int quantity)
     {
@@ -86,17 +94,20 @@ public class ShoppingCartController
 
     // add a DELETE method to clear all products from the current users cart
     // https://localhost:8080/cart  - return the (now empty) cart so the front end can refresh it (200 OK)
-    @DeleteMapping("/cart")
+    @DeleteMapping("")
     @PreAuthorize("hasRole('ROLE_USER')")
-    public ShoppingCart deleteCart(Principal principal, @RequestBody ShoppingCart shoppingCart)
+//    @PreAuthorize("hasRole('USER')")
+//    public ShoppingCart deleteCart(Principal principal, @RequestBody ShoppingCart shoppingCart)
+    public ResponseEntity<ShoppingCart> deleteCart(Principal principal, @RequestBody ShoppingCart ignoredBody)
     {
-        shoppingCart = getCart(principal);
         String userName = principal.getName();
         User user = userService.getByUserName(userName);
         int userId = user.getId();
-        shoppingCartService.deleteCart(userId);
 
-        return shoppingCart;
+        ShoppingCart cart = getCart(principal);
+        ShoppingCart saved = shoppingCartService.deleteCart(userId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(saved);
     }
 
 }
--- src/main/java/org/yearup/service/ShoppingCartService.java
+++ src/main/java/org/yearup/service/ShoppingCartService.java
@@ -1,13 +1,21 @@
 package org.yearup.service;
 
+import jakarta.persistence.Column;
+import org.springframework.data.web.config.SpringDataWebSettings;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import org.yearup.models.CartItem;
 import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
 import org.yearup.models.ShoppingCartItem;
 import org.yearup.repository.ShoppingCartRepository;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class ShoppingCartService
@@ -15,11 +23,13 @@ public class ShoppingCartService
     // a shopping cart is built from cart rows plus a product lookup for each row
     private final ShoppingCartRepository shoppingCartRepository;
     private final ProductService productService;
+    private final SpringDataWebSettings springDataWebSettings;
 
-    public ShoppingCartService(ShoppingCartRepository shoppingCartRepository, ProductService productService)
+    public ShoppingCartService(ShoppingCartRepository shoppingCartRepository, ProductService productService, SpringDataWebSettings springDataWebSettings)
     {
         this.shoppingCartRepository = shoppingCartRepository;
         this.productService = productService;
+        this.springDataWebSettings = springDataWebSettings;
     }
 
     public ShoppingCart getByUserId(int userId)
@@ -31,6 +41,7 @@ public class ShoppingCartService
             int productId = item.getProductId();
             ShoppingCartItem shoppingCartItem = new ShoppingCartItem();
             shoppingCartItem.setProduct(productService.getById(productId));
+            shoppingCartItem.setQuantity(item.getQuantity()); // Getting quantity or else it will always be 1
             shoppingCart.add(shoppingCartItem);
         }
 
@@ -40,13 +51,44 @@ public class ShoppingCartService
 
     // add additional methods here
 
-    public ShoppingCart addToCart(ShoppingCart shoppingCart, int productId)
+//    public ShoppingCart create(ShoppingCart shoppingCart)
+//    {
+//        return shoppingCartRepository.save(shoppingCart);
+//    }
+
+    public ShoppingCart addToCart(ShoppingCart cart, int productId, int userId)
     {
-        ShoppingCart cart = shoppingCart;
-        int id = productId;
+        Product product = productService.getById(productId);
+        if (product == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        CartItem existingItem = shoppingCartRepository.findByUserIdAndProductId(userId, productId);
         ShoppingCartItem shoppingCartItem = new ShoppingCartItem();
-        shoppingCartItem.setProduct(productService.getById(productId));
-        shoppingCart.add(shoppingCartItem);
+        shoppingCartItem.setProduct(product);
+
+        if (existingItem != null) {
+            int existingQuantity = existingItem.getQuantity();
+//            System.out.println("\n\nEXISTING QUANTITY: ");
+//            System.out.println(existingQuantity);
+            int newQuantity = existingQuantity + 1;
+//            System.out.println("\n\nNEW QUANTITY: ");
+//            System.out.println(newQuantity);
+            existingItem.setQuantity(newQuantity);
+            shoppingCartItem.setQuantity(newQuantity);
+            shoppingCartRepository.save(existingItem);
+
+        } else {
+            CartItem newItem = new CartItem();
+            newItem.setCartItemId(0);
+            newItem.setUserId(userId);
+            newItem.setProductId(productId);
+            newItem.setQuantity(1);
+            shoppingCartItem.setQuantity(1);
+            shoppingCartRepository.save(newItem);
+        }
+
+        cart.add(shoppingCartItem);
 
         return cart;
     }
@@ -60,9 +102,12 @@ public class ShoppingCartService
         return cart;
     }
 
-    public void deleteCart(int userId) {
+    public ShoppingCart deleteCart(int userId) {
 
         shoppingCartRepository.deleteByUserId(userId);
 
+        ShoppingCart cart = new ShoppingCart();
+
+        return cart;
     }
 }
